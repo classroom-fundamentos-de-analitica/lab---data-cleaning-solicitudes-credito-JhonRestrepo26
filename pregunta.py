4532348e7d2151bac54bb1f12bfe847292d140c2
Jhon Alexander Restrepo Henao <112440657+JhonRestrepo26@users.noreply.github.com>
2022-11-28 17:53:54 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -16,5 +16,30 @@ def clean_data():
     #
     # Inserte su código aquí
     #
+    
+    #Columna monto:
 
+    df.monto_del_credito = df.monto_del_credito.str.strip('$')
+    df.monto_del_credito = df.monto_del_credito.str.replace('\.00','')
+    df.monto_del_credito = df.monto_del_credito.str.replace(',','')
+    
+    #Fechas:
+
+    df.fecha_de_beneficio=pd.to_datetime(df.fecha_de_beneficio)
+    
+    #Comunas como entero
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
+    
+    # Minúsculas todo lo que sea texto:
+
+    col_texts=['sexo','tipo_de_emprendimiento','idea_negocio','barrio','línea_credito']
+
+    for i in col_texts:
+      df[i]=df[i].str.lower()
+    
+    #Eliminar duplicados y NAN's
+
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
+    
     return df
